0b349de2ae82e5dacd221547d5aa14fc3bb80640
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2023-09-25 19:36:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(record) {
+    const resultObject = record.find(fetchArr);
+
+    if (resultObject !== undefined) {
+        return resultObject.year;
+    }
+
+    return resultObject;
+}
+
+function fetchArr(arr) {
+    return arr.result === "W"
+}
